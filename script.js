058192531785e2888f42b33857fe25cea370390c
chandrikaHarsha <chandrikaharsha610@gmail.com>
2024-02-11 11:15:08 +0530

    PopX assignment

---
+++ script.js
@@ -0,0 +1,8 @@
+let newUser = document.getElementById("newAcc");
+newUser.addEventListener("click", () => {
+  window.location.href = "PopXRegistrationForm.html"
+});
+let existingUser = document.getElementById('login');
+existingUser.addEventListener('click', ()=>{
+    window.location.href = 'Login.html'
+})
